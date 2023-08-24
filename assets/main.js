c1b14aaeba884ebfc2f85d7cdead96c052140ffd
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-08-24 15:36:13 +0200

    add input and button

--- assets/main.js
+++ assets/main.js
@@ -5,4 +5,15 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.*/
 
+//chiedere all'utente nome, chilometri da percorrere ed età
 
+const userName = document.querySelector('#name').value;
+const kmUser = document.querySelector('#km').value;
+const userAge = document.querySelector('#age').value;
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+    window.location.reload();
+})
+
+console.log(userName,kmUser,userAge);
